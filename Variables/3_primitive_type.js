13e619d76efa62e84d2312f00af949b5c31f15ce
Carter Do <163492013+EggBoi1er@users.noreply.github.com>
2025-10-09 21:38:40 +0000

    new files

---
+++ Variables/3_primitive_type.js
@@ -0,0 +1,23 @@
+// 1. String
+let fname = 'Bronco';
+let lname = 'CPP';
+let fullName = `${fname} ${lname}`
+console.log(fullName)
+
+// 2. Number
+let evenNumber = 10;
+let pi = 3.14;
+console.log(typeof: pi)
+
+// 3. boolean
+let hasTicket = true;
+let passExam = false;
+console.log(typeof passExam)
+
+// 4. undefined
+let index;
+console.log(typeof index)
+
+
+
+const secretKey = symbol();
