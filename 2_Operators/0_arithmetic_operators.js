34f4fbbf6e507cfbbed9bfb3963da6db5c2b0972
Carter Do <163492013+EggBoi1er@users.noreply.github.com>
2025-10-14 21:44:35 +0000

    save local edits before rebase

--- 2_Operators/0_arithmetic_operators.js
+++ 2_Operators/0_arithmetic_operators.js
@@ -12,4 +12,15 @@ PEMDAS
 console.log(2 ** -1);
 console.log(1 / 0);
 
+// shortcuts
+let number = 1;
+number++; // 2 returns the value and then increments after
+number--; // 1 returns the value and then decrements after
+number += 5; // 6
+number -= 2; // 4
+number *= 3; // 12
+
+++number; // 13 increments first then returns the value
+--number; // 12 decrements first then returns the value
+console.log(number);
 
