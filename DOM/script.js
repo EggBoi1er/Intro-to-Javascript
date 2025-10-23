8b8cddd03cf30b4aed1ed0e7ef40119d632ba08b
Carter Do <163492013+EggBoi1er@users.noreply.github.com>
2025-10-23 15:44:04 -0700

    fixed dom and js file for dom

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,4 @@
-const h1 = document.getElementById("heading");
+const h1 = document.getElementById("heading"); // this will return null if the script is in the head without defer
+// also returns if there is no element with that id
+// use querySelector to get by class or tag name so instead of heading you can do ".heading" or "h1"
 h1.textContent = "Hello CS 2250, from JS";
